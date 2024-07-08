47b017ee9c943a175d2049c8d0d1a7e5d7e3d380
teresabat <contatotommymateus@gmail.com>
2024-07-08 17:15:31 -0300

    Aprendendo até booleanos

---
+++ Hello/hello.go
@@ -0,0 +1,13 @@
+// pacote principal
+package main
+
+import "fmt"
+
+// função principal
+func main() {
+
+	fmt.Println("Hello, world!")
+
+}
+
+//imprimir na tela Hello world
---
+++ numero/numero.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("2+3 = ", 2+3)
+}
